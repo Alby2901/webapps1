2250c9cee1f4c7e29e56b6cbb4b53baaf2f8e792
Alby2901 <a.azzola@scriptanet.it>
2022-04-20 17:59:14 +0200

    Exercise 1.1 OK

--- weeks/week01/improve_scores.js
+++ weeks/week01/improve_scores.js
@@ -2,16 +2,65 @@
 
 const scores = [28, 25, 21, 30, 18, 24, 22];
 
-console.log(scores.join(', '));
+let avg = 0;
+for (const x of scores) {
+    avg += x;
+}
+avg = Math.round(avg/scores.length);
 
-// find the minimun
+
+console.log('scores     = ' + scores.join(', '));
+console.log('avg of score  = ' + avg);
+
+const scores_new = [...scores];
+
+console.log('scores_new = ' + scores_new.join(', '));
+
+// find the first minimun
 let min_pos = 0
-for (let i = 0; i < scores.length; i++) {
-    if (scores[i] < scores[min_pos])
+for (let i = 0; i < scores_new.length; i++) {
+    if (scores_new[i] < scores_new[min_pos])
+        min_pos = i;
+}
+
+console.log('first score min to remove = ' + scores_new[min_pos]);
+
+// eliminate the first min
+scores_new.splice(min_pos,1);
+
+console.log('scores_new = ' + scores_new.join(', '));
+
+// find the second minimun
+min_pos = 0
+for (let i = 0; i < scores_new.length; i++) {
+    if (scores_new[i] < scores_new[min_pos])
         min_pos = i;
 
 }
 
-console.log(scores[min_pos]);
+console.log('second score min to remove = ' + scores_new[min_pos]);
 
-console.log(scores.join(', '));
+// eliminate the socond min
+scores_new.splice(min_pos,1);
+
+console.log('scores_new = ' + scores_new.join(', '));
+
+let avg_new = 0;
+for (const x of scores_new) {
+    avg_new += x;
+}
+avg_new = Math.round(avg_new/scores_new.length);
+
+scores_new.push(avg_new);
+scores_new.push(avg_new);
+
+console.log('scores_new = ' + scores_new.join(', '));
+
+avg_new = 0;
+for (const x of scores_new) {
+    avg_new += x;
+}
+avg_new = Math.round(avg_new/scores_new.length);
+
+// console.log('scores_new            = ' + scores_new.join(', '));
+console.log('avg_new of score_new  = ' + avg_new);
