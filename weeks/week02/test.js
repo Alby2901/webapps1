98e272567a2eae1385acdd5168cb414c435d8254
Alby2901 <a.azzola@scriptanet.it>
2022-04-29 17:17:05 +0200

    Test HTML Canvas 01

--- weeks/week02/test.js
+++ weeks/week02/test.js
@@ -1,5 +1,22 @@
 'use strict'
 
-console.log('pippo')
+function greeter(name) {
+    const myname = name;
+
+    const hello = function () {
+        return "Ciao " + myname;
+    }
+return hello;
+
+}
+
+const helloTom = greeter("Tom");
+const helloJay = greeter("Jay");
+
+
+console.log(helloTom());
+console.log(helloJay());
+
+console.log('Programma terminato');
 
 
